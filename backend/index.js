6f753df289f9a147f1601dcd85db0fe3306d1f4c
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-15 14:04:00 +0300

    Backend <> Nodejs project: adding all the requirment to run the server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, (err)=>{
+    if(err) console.log(err);
+    console.log(`Running server on port ${process.env.PORT}`);
+})
