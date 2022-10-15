fcacef2228dcfbe4b235b3202104a63660612f89
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-15 19:26:08 +0300

    Backend <> Jobs table: creating job model

---
+++ backend/models/jobs.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: 'title is required'
+    },
+    company_name: {
+        type: String,
+        required: 'company name is required'
+    },
+    workplace: {
+        type: String,
+        required: 'workplace is required'
+    },
+    location: {
+        type: String,
+        required: 'location is required'
+    },
+    employment: {
+        type: String,
+        required: 'employment is required'
+    },
+    description: {
+        type: String,
+        required: 'description is required'
+    }
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
