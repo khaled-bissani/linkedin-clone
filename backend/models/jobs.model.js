925c0053898f31f97c9b33f47cea56af2886aa1a
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-16 19:55:23 +0300

    Backend <> Job controller: creating the api for new job

--- backend/models/jobs.model.js
+++ backend/models/jobs.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('../models/users.model');
 
 const jobSchema = new mongoose.Schema({
     title: {
@@ -24,7 +25,11 @@ const jobSchema = new mongoose.Schema({
     description: {
         type: String,
         required: 'description is required'
-    }
+    },
+    user_id: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
 })
 
 jobSchema.index({'$**': 'text'});
