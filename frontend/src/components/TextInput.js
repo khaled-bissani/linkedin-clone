9807b1e57108b87bcd5533edb5d85811872ed3ec
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-17 10:28:43 +0300

    Frontend <> Signup: handling first name

--- frontend/src/components/TextInput.js
+++ frontend/src/components/TextInput.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useState } from 'react';
 
-const TextInput = ({type,placeholder}) => {
-
-  const [value, setValue] = useState('');
-
-  const handleValue= event => {
-    setValue(event.target.value);
-  }
-  console.log(value)
+const TextInput = ({type,placeholder,value,onChange}) => {
 
   return (
     <div>
-        <input value={value} onChange={handleValue} className="border rounded-md outline-none w-[300px] h-[40px] pl-4" type={type} placeholder={placeholder} />
+        <input value={value} onChange={onChange} className="border rounded-md outline-none w-[300px] h-[40px] pl-4" type={type} placeholder={placeholder} />
     </div>
   )
 }
--- frontend/src/signup/Signup.js
+++ frontend/src/signup/Signup.js
@@ -1,15 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
 import SubmitButton from '../components/SubmitButton'
 import TextInput from '../components/TextInput'
 
 const Signup = () => {
+    
+    const [users, setUsers] = useState({
+        user: {
+          first_name: '',
+          last_name: '',
+          email: '',
+          password: '',
+          country: '',
+          city: '',
+          user_type:''
+        }
+      });
+
+      function handleFirstNameChange(e) {
+        setUsers({
+          ...users,
+          user: {
+            ...users.user,
+            first_name: e.target.value
+          }
+        });
+      }
+
   return (
     <div className='bg-[#f3f2ee] h-screen p-10'>
         <img src={require('../assets/logo.PNG')} alt={"logo"}/>
         <div className='flex flex-col items-center bg-[#f3f2ee]'>
             <h1 className='text-3xl font-bold tracking-wide mb-5'>Make the most of your professional life</h1>
             <div className='flex flex-col items-center justify-around border w-[500px] h-[500px] bg-white'>
-                <TextInput type={"text"} placeholder={"First name"} />
+                <TextInput type={"text"} placeholder={"First name"} value={users.user.first_name} onChange={handleFirstNameChange}/>
                 <TextInput type={"text"} placeholder={"Last name"} />
                 <TextInput type={"text"} placeholder={"Email"} />
                 <TextInput type={"text"} placeholder={"Password"} />
