7720837a5e419c8094e01bb65ddab72bdda9c50f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-17 10:29:52 +0300

    Frontend <> Signup: handling last name

--- frontend/src/signup/Signup.js
+++ frontend/src/signup/Signup.js
@@ -27,6 +27,16 @@ const Signup = () => {
         });
       }
 
+      function handleLastNameChange(e) {
+        setUsers({
+          ...users,
+          user: {
+            ...users.user,
+            last_name: e.target.value
+          }
+        });
+      }
+
   return (
     <div className='bg-[#f3f2ee] h-screen p-10'>
         <img src={require('../assets/logo.PNG')} alt={"logo"}/>
@@ -34,7 +44,7 @@ const Signup = () => {
             <h1 className='text-3xl font-bold tracking-wide mb-5'>Make the most of your professional life</h1>
             <div className='flex flex-col items-center justify-around border w-[500px] h-[500px] bg-white'>
                 <TextInput type={"text"} placeholder={"First name"} value={users.user.first_name} onChange={handleFirstNameChange}/>
-                <TextInput type={"text"} placeholder={"Last name"} />
+                <TextInput type={"text"} placeholder={"Last name"}  value={users.user.last_name} onChange={handleLastNameChange}/>
                 <TextInput type={"text"} placeholder={"Email"} />
                 <TextInput type={"text"} placeholder={"Password"} />
                 <TextInput type={"text"} placeholder={"Country"} />
