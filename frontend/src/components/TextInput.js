aa3d1e040e53e5783383293ab7aaab3ddb111144
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-17 08:25:25 +0300

    Frontend <> React project: creating text input component

---
+++ frontend/src/components/TextInput.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react';
+
+const TextInput = ({type,placeholder}) => {
+
+  const [value, setValue] = useState('');
+
+  const handleValue= event => {
+    setValue(event.target.value);
+  }
+  console.log(value)
+
+  return (
+    <div>
+        <input value={value} onChange={handleValue} className="border rounded-md w-[300px] pl-4" type={type} placeholder={placeholder} />
+    </div>
+  )
+}
+
+export default TextInput
