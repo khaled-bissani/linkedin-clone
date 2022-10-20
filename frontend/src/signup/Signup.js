e6fd6f1b270c095a6767f987bee58c1f654dc1c0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-20 14:30:07 +0300

    Frontend <> Signup: editing the handling of the input fields

--- frontend/src/signup/Signup.js
+++ frontend/src/signup/Signup.js
@@ -8,8 +8,7 @@ const Signup = () => {
 
     const navigate = useNavigate();
     
-    const [users, setUsers] = useState({
-        user: {
+    const [user, setUser] = useState({
           first_name: '',
           last_name: '',
           email: '',
@@ -17,81 +16,17 @@ const Signup = () => {
           country: '',
           city: '',
           user_type:''
-        }
+       
       });
-
-      const handleFirstNameChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            first_name: e.target.value
-          }
-        });
-      }
-
-      const handleLastNameChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            last_name: e.target.value
-          }
-        });
-      }
-
-      const handleEmailChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            email: e.target.value
-          }
-        });
-      }
-
-      const handlePasswordChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            password: e.target.value
-          }
-        });
-      }
-
-      const handleCountryChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            country: e.target.value
-          }
-        });
-      }
-
-      const handleCityChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            city: e.target.value
-          }
-        });
-      }
-
-      const handleUserTypeChange=(e)=> {
-        setUsers({
-          ...users,
-          user: {
-            ...users.user,
-            user_type: e.target.value
-          }
+      
+      const handleChange=(e,feild)=> {
+        setUser({
+          ...user, [`${feild}`]: e.target.value
         });
       }
 
       const handleClick = () => {
-        console.log(users.user)
+        console.log(user)
         navigate("/home")
       }
 
@@ -101,13 +36,13 @@ const Signup = () => {
         <div className='flex flex-col items-center bg-[#f3f2ee]'>
             <h1 className='text-3xl font-bold tracking-wide mb-5'>Make the most of your professional life</h1>
             <div className='flex flex-col items-center justify-around border w-[500px] h-[500px] bg-white'>
-                <TextInput type={"text"} placeholder={"First name"} value={users.user.first_name} onChange={handleFirstNameChange}/>
-                <TextInput type={"text"} placeholder={"Last name"}  value={users.user.last_name} onChange={handleLastNameChange}/>
-                <TextInput type={"text"} placeholder={"Email"} value={users.user.email} onChange={handleEmailChange}/>
-                <TextInput type={"password"} placeholder={"Password"} value={users.user.password} onChange={handlePasswordChange}/>
-                <TextInput type={"text"} placeholder={"Country"} value={users.user.country} onChange={handleCountryChange}/>
-                <TextInput type={"text"} placeholder={"City"} value={users.user.city} onChange={handleCityChange}/>
-                <select className='outline-none' value={users.user.user_type} onChange={handleUserTypeChange}>
+                <TextInput type={"text"} placeholder={"First name"} value={user.first_name} onChange={(e)=>handleChange(e,"first_name")}/>
+                <TextInput type={"text"} placeholder={"Last name"}  value={user.last_name} onChange={(e)=>handleChange(e,"last_name")}/>
+                <TextInput type={"text"} placeholder={"Email"} value={user.email} onChange={(e)=>handleChange(e,"email")}/>
+                <TextInput type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")}/>
+                <TextInput type={"text"} placeholder={"Country"} value={user.country} onChange={(e)=>handleChange(e,"country")}/>
+                <TextInput type={"text"} placeholder={"City"} value={user.city} onChange={(e)=>handleChange(e,"city")}/>
+                <select className='outline-none' value={user.user_type} onChange={(e)=>handleChange(e,"user_type")}>
                     <option>User Type</option>
                     <option value={1}>Company</option>
                     <option value={2}>Normal User</option>
