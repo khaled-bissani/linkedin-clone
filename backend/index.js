d451116e04364f3ee856c632e1420fb4b164fc03
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-20 15:24:31 +0300

    Backend <> NodeJS: adding cors in nodejs

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ require('./config/database.config');
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+app.use(cors());
+
 const authRouter = require('./routes/auth.routes');
 app.use('/auth', authRouter);
 
